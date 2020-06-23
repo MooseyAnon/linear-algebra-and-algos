50062c1490a42c940b236e472afa1232c4a76086
MooseyAnon <moooseyanon@gmail.com>
2020-06-23 18:33:27 +0100

    algorithms->c: 2D peak finding
    This commit implements a working 2D peak finder.

--- src/algorithms/c/binarySearch.c
+++ src/algorithms/c/binarySearch.c
@@ -1,5 +1,13 @@
 #include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct max_vector {
+    unsigned int col_index;
+    unsigned int row_index;
+    int index_value;
+} max_vector;
+
 
 int binary_search(int arr[], int first, int last, int item) {
     /* A recursive binary search */
@@ -22,16 +30,115 @@ int binary_search(int arr[], int first, int last, int item) {
     }
 }
 
+
+max_vector * find_col_peak(int *arr, int rows, int col){
+    /* Find the peak of a column.
+    
+    This function treats a 2D array like a 1D array and
+    traverses using some basic pointer arithmetic.
+
+    next item = *((i * row + arr) + j)
+    where:
+        - i == current row number (the y axis in matrix i.e. up and down)
+        - j == column number in column space (the x axis i.e left to right)
+        - row == the number of items in each row
+        - *arr == the first item in the array
+    */
+
+    // we want a vector to save the max number and its index
+    max_vector *vector = malloc(sizeof(max_vector));
+    
+    if (vector) {
+        // set max to the first item of the column
+        vector->col_index = 0;
+        vector->index_value = *(col + arr);
+    
+        // loop through the rest of the column
+        for (int i=1; i < rows; i++){
+            int next_number = *((i * rows + arr) + col);
+            if (next_number > vector->index_value){
+                vector->col_index = i;
+                vector->index_value = next_number;
+            }
+        }
+        printf("the max value is %d and it is at index %d\n",
+            vector->index_value, vector->col_index);
+        return vector;
+    }
+    return NULL;
+}
+
+
+max_vector * peak_finder(int *matrix, int rows, int first, int last){
+    /* Find a local 2D peak.
+
+    NOTE: Expects a matrix of size row x col
+
+    A peak is defined as:
+    (i, j) >= (i, j + 1), (i, j - 1), (i + 1, j) (i - 1, j)
+    */
+
+    max_vector *vector;
+    
+    int middle = last / 2;
+    vector = find_col_peak(matrix, rows, middle);
+
+    if (vector){
+        if (middle == first || middle == last){
+            vector->row_index = middle;
+            return vector;
+        }
+
+        if (last > first) {
+            int current = *((vector->col_index * rows) + matrix + middle);
+            int one_less = *((vector->col_index * rows) + matrix + middle - 1);
+            int one_more = *((vector->col_index * rows) + matrix + middle + 1);
+
+            if (one_less <= current >= one_more) {
+                vector->row_index = middle;
+                return vector;
+            }
+
+            // if current is not a peak we can free the vector
+            if (one_less > current){
+                free(vector);
+                return peak_finder(matrix, rows, first, middle - 1);
+            } else {
+                free(vector);
+                return peak_finder(matrix, rows, middle + 1, last);
+            } 
+        }
+        vector->row_index = middle;
+        return vector;
+    }
+    return NULL;
+}
+
+
 int main(void) {
     printf("Hello World\n");
 
     int myarr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-    // --------- tests -------
+    // --------- binary search tests -------
     assert(binary_search(myarr, 0, 10, 5) == 4);
     assert(binary_search(myarr, 0, 10, 1) == 0);
     assert(binary_search(myarr, 0, 10, 10) == 9);
     assert(binary_search(myarr, 0, 10, 0) == -1);
     assert(binary_search(myarr, 0, 10, -2) == -1);
+
+    // ------------ 2D peak ----------
+
+    int arr [5][5] = {
+        {10, 8, 11, 10, 16},
+        {14, 13, 12, 11, 24},
+        {15, 9, 11, 21, 6},
+        {16, 21, 19, 20, 12},
+        {100, 6, 13, 19, 9},
+    };
+
+    max_vector *ptr = find_col_peak(*arr, 5, 3);
+    printf("the local peak is at %d, %d", ptr->row_index, ptr->col_index);
+    free(ptr);
     return 0;
 }
